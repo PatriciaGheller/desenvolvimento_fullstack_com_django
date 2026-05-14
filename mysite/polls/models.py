36797f4f05714ceaeb72e69299e63f7011cb1e43
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-14 18:06:55 -0300

    Ajusta models e migrações: remove campo x de Question e atualiza dependências

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,24 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')  
     active = models.BooleanField(default=True)
-    x = models.TextField(default='')
     
+    
+    def was_published_recently(self):
+        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
+    
+    def __str__(self):
+        return self.question_text
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f'{self.question.id}:{self.choice_text}'
