a531347304597652922d3bb97c3b5efefce9b7a2
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-05-16 09:34:19 -0300

    Ajuste na view de detalhe para carregamento dinâmico de templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,7 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, render
-from django.http import Http404
-from django.http import HttpResponse
-from polls.models import Question
-
+from django.urls import reverse
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -22,4 +21,21 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Você não selecionou nenhuma opção.",
+            },
+        )
+    
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        
